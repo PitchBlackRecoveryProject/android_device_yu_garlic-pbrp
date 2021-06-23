a1a7c84b7456632ed569ba579fd797e20e600825
DrtSinX98 <pritish@stag-os.org>
2021-06-23 20:37:46 +0200

    garlic: PBRP bringup

--- omni_garlic.mk
+++ omni_garlic.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, vendor/omni/config/gsm.mk)
+#$(call inherit-product, vendor/omni/config/gsm.mk)
 
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Encryption
 PRODUCT_PACKAGES += \
